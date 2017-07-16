e29dd68b94f62a1f3159f85d18ea5dd8b5f8beea
androks <androks12@gmail.com>
2017-07-16 12:35:06 +0300

    Wrote contactsContract

---
+++ app/src/main/java/com/androks/contactstest/contacts/ContactsContract.java
@@ -0,0 +1,39 @@
+package com.androks.contactstest.contacts;
+
+import com.androks.contactstest.BasePresenter;
+import com.androks.contactstest.BaseView;
+import com.androks.contactstest.data.Contact;
+
+import java.util.List;
+
+/**
+ * Created by androks on 16.07.17.
+ */
+
+public class ContactsContract {
+
+    interface View extends BaseView<Presenter>{
+
+        void setLoadingIndicator(boolean active);
+
+        void showContacts(List<Contact> contacts);
+
+        void showAddContact();
+
+        void showNoContacts();
+
+
+    }
+
+
+    interface Presenter extends BasePresenter {
+
+        void result(int requestCode, int resultCode);
+
+        void loadContacts();
+
+        void addNewContact();
+
+        void openContactDetails();
+    }
+}
